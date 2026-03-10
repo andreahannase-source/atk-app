64ce1c5957d7d6bfa1bceb63f9a08a9e3bdeff01
WORKSTATION <andrea.hanna.se@gmail.com>
2026-03-10 12:46:43 +0100

    Remove Cors config

---
+++ assets/LocationCity-CEf8XKKG.js
@@ -0,0 +1,1 @@
+import{k as h,j as m}from"./index-MD8EeOvQ.js";const o=h(m.jsx("path",{d:"M15 11V5l-3-3-3 3v2H3v14h18V11zm-8 8H5v-2h2zm0-4H5v-2h2zm0-4H5V9h2zm6 8h-2v-2h2zm0-4h-2v-2h2zm0-4h-2V9h2zm0-4h-2V5h2zm6 12h-2v-2h2zm0-4h-2v-2h2z"}),"LocationCity");export{o as default};
---
+++ assets/Remove-D9kQtGqV.js
@@ -0,0 +1,1 @@
+import{k as e,j as o}from"./index-MD8EeOvQ.js";const t=e(o.jsx("path",{d:"M19 13H5v-2h14z"}),"Remove");export{t as R};
